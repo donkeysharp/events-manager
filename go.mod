7afa1d87343b613506406f32eaee4c41f7c3130e
Sergio Guillen <serguimant@gmail.com>
2021-02-22 23:22:03 -0400

    Add Gorm example

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/donkeysharp/events-manager
 go 1.15
 
 require (
-	github.com/lib/pq v1.2.0
+	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
+	github.com/lib/pq v1.3.0
 	github.com/rubenv/sql-migrate v0.0.0-20210215143335-f84234893558
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gorm.io/driver/postgres v1.0.8
+	gorm.io/gorm v1.20.12
 )
--- main.go
+++ main.go
@@ -4,33 +4,84 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/donkeysharp/events-manager/db/migrations"
 	_ "github.com/lib/pq"
 	migrate "github.com/rubenv/sql-migrate"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func main() {
-	fmt.Println("Online Events Manager")
+func testMigration(connectionString string, direction migrate.MigrationDirection) {
 	fmt.Println(migrations.AssetNames())
 	migration := &migrate.AssetMigrationSource{
 		Asset:    migrations.Asset,
 		AssetDir: migrations.AssetDir,
 		Dir:      "db/migrations",
 	}
-	var connectionString string
-	// TODO: Create configuration loader
-	connectionString = "postgres://test:12345@localhost:5432/events_db?sslmode=disable"
 
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		panic(err)
 	}
 
-	result, err := migrate.Exec(db, "postgres", migration, migrate.Up)
+	result, err := migrate.Exec(db, "postgres", migration, direction)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Migration result %d\n", result)
+}
+
+type event struct {
+	gorm.Model
+	Name        string
+	Slug        string
+	Description string
+}
+
+func testGorm(connectionString string) {
+	fmt.Printf("Test Gorm with connection string %s\n", connectionString)
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Creating an event...")
+	db.Create(&event{Name: "event1", Slug: "slug_event_1", Description: "description"})
+
+	fmt.Println("Retrieving all events...")
+	var events []event
+	result := db.Find(&events)
+	fmt.Println(result.RowsAffected)
+	fmt.Println(events[0].Slug)
+}
+
+func main() {
+	var directionMap = make(map[string]migrate.MigrationDirection)
+	directionMap["up"] = migrate.Up
+	directionMap["down"] = migrate.Down
+
+	fmt.Println("Online Events Manager")
+	migrateCmd := flag.NewFlagSet("migrate", flag.ExitOnError)
+	direction := migrateCmd.String("direction", "up", "Direction of the migration up/down")
+
+	startCmd := flag.NewFlagSet("start", flag.ExitOnError)
+	command := os.Args[1]
+
+	var connectionString string
+	connectionString = "postgres://test:12345@localhost:5432/events_db?sslmode=disable"
+
+	switch command {
+	case "migrate":
+		migrateCmd.Parse(os.Args[2:])
+		fmt.Printf("Migrating database. Direction: %s\n", *direction)
+		testMigration(connectionString, directionMap[strings.ToLower(*direction)])
+	case "start":
+		startCmd.Parse(os.Args[2:])
+		testGorm(connectionString)
+	}
 }
